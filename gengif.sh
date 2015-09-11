865174d5e92bf30c89f08e79c4c376412f62d342
chaosink <475936052@qq.com>
2015-09-11 16:59:43 +0800

    done

--- gengif.sh
+++ gengif.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-ffmpeg -f rawvideo -pixel_format rgb24 -video_size 512x512 -framerate $1 -i raw_video -vf "vflip" result.gif
+if [ $# -ne 1 ]; then
+	echo "Usage: gengif.sh fps"	
+else
+	ffmpeg -f rawvideo -pixel_format rgb24 -video_size 512x512 -framerate $1 -i raw_video -vf "vflip" result.gif
+fi
